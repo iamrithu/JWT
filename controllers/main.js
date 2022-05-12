0dd93f9d9aadbb6787fa63725eceecda24fea5ce
Rithi Mahesh Kumar <92303214+iamrithu@users.noreply.github.com>
2022-05-12 20:26:47 +0530

    Update main.js

--- controllers/main.js
+++ controllers/main.js
@@ -1,26 +1,19 @@
-// check username, password in post(login) request
-// if exist create new JWT
-// send back to fron-end
-// setup authentication so only the request with JWT can access the dasboard
+
 
 const jwt = require('jsonwebtoken')
 const { BadRequestError } = require('../errors')
 
 const login = async (req, res) => {
   const { username, password } = req.body
-  // mongoose validation
-  // Joi
-  // check in the controller
+  
 
   if (!username || !password) {
     throw new BadRequestError('Please provide email and password')
   }
 
-  //just for demo, normally provided by DB!!!!
+ 
   const id = new Date().getDate()
 
-  // try to keep payload small, better experience for user
-  // just for demo, in production use long, complex and unguessable string value!!!!!!!!!
   const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
     expiresIn: '30d',
   })
